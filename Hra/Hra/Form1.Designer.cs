fdcf2266fe7ed74d79ded6f9b541b3b58b45fabc
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2022-01-25 10:45:08 +0100

    Nahrání

---
+++ Hra/Hra/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Hra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Hrac = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.Hrac.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.Hrac);
+            this.panel1.Location = new System.Drawing.Point(0, 164);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 200);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(206, 164);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 200);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Location = new System.Drawing.Point(412, 164);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 200);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Location = new System.Drawing.Point(0, 370);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 200);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Gray;
+            this.panel5.Location = new System.Drawing.Point(206, 370);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 200);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Gray;
+            this.panel6.Location = new System.Drawing.Point(412, 370);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 200);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Gray;
+            this.panel9.Location = new System.Drawing.Point(412, 576);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(200, 200);
+            this.panel9.TabIndex = 8;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Gray;
+            this.panel8.Location = new System.Drawing.Point(206, 576);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(200, 200);
+            this.panel8.TabIndex = 7;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Gray;
+            this.panel7.Location = new System.Drawing.Point(0, 576);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 200);
+            this.panel7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(212, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 39);
+            this.label1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Hrac
+            // 
+            this.Hrac.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Hrac.Controls.Add(this.label2);
+            this.Hrac.Location = new System.Drawing.Point(60, 60);
+            this.Hrac.Name = "Hrac";
+            this.Hrac.Size = new System.Drawing.Size(80, 80);
+            this.Hrac.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(15, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Hráč";
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(12, 12);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(588, 25);
+            this.panel10.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Spustit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 777);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Pozice 1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.Hrac.ResumeLayout(false);
+            this.Hrac.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel Hrac;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Hra/Hra/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra
+{
+    public partial class Form1 : Form
+    {
+        int pozice = 1;
+        int cas = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rng = new Random();
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.D)
+            {
+                if(pozice!= 3 && pozice != 6 && pozice != 9)
+                    pozice++;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                if (pozice != 1 && pozice != 4 && pozice != 7)
+                    pozice--;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if (pozice < 7)
+                    pozice += 3;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                if (pozice > 3)
+                    pozice-=3;
+            }
+
+            switch(pozice)
+            {
+                case 1:
+                    panel1.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 2:
+                    panel2.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 3:
+                    panel3.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 4:
+                    panel4.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 5:
+                    panel5.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 6:
+                    panel6.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 7:
+                    panel7.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 8:
+                    panel8.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+                case 9:
+                    panel9.Controls.Add(this.Hrac);
+                    Hrac.Location = new Point(60, 60);
+                    break;
+            }
+            this.Text = "Pozice " + pozice;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (cas < 0)
+            {
+                timer1.Stop();
+                cas = 5;
+                losovani();
+            }
+            else
+            {
+                label1.Text = "Losování za " + cas;
+                cas--;
+            }
+        }
+
+        public void losovani()
+        {
+            int rndm = rng.Next(1, 10);
+            switch (rndm)
+            {
+                case 1:
+                    panel1.BackColor = Color.DimGray;
+                    break;
+                case 2:
+                    panel2.BackColor = Color.DimGray;
+                    break;
+                case 3:
+                    panel3.BackColor = Color.DimGray;
+                    break;
+                case 4:
+                    panel4.BackColor = Color.DimGray;
+                    break;
+                case 5:
+                    panel5.BackColor = Color.DimGray;
+                    break;
+                case 6:
+                    panel6.BackColor = Color.DimGray;
+                    break;
+                case 7:
+                    panel7.BackColor = Color.DimGray;
+                    break;
+                case 8:
+                    panel8.BackColor = Color.DimGray;
+                    break;
+                case 9:
+                    panel9.BackColor = Color.DimGray;
+                    break;
+            }
+            if(rndm == pozice)
+            {
+                label1.Text = "Game over";
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
